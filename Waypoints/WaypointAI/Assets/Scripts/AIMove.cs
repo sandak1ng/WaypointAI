aa41ebc9edbbe917b22c8af40010190a820755f8
sandak1ng <dsan.work@gmail.com>
2018-01-06 15:23:55 +0000

    added new stage with new techinque used

---
+++ Waypoints/WaypointAI/Assets/Scripts/AIMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour {
+
+	public Transform destinationPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = destinationPoint.position;
+	}
+}
---
+++ Waypoints/WaypointAI/Assets/Scripts/AIMove2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove2 : MonoBehaviour {
+
+	public Transform[] Waypoint;
+	public float speed;
+
+	private int current;
+	
+	// Update is called once per frame
+	void Update () {
+		//Keeps moving until you reach the current waypoint
+		if (transform.position != Waypoint [current].position) {
+			Vector3 pos = Vector3.MoveTowards (transform.position, Waypoint [current].position, speed * Time.deltaTime);
+			//Waypoint reached, moved onto the next object
+			GetComponent<Rigidbody> ().MovePosition (pos);
+		} else
+			current = (current + 1) % Waypoint.Length;
+	}
+}
